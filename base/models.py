931e3aaa43ff831c2616ac6d348aa4905388238c
Minh Hung <minhhung.nguyen@sjsu.edu>
2022-03-27 16:47:28 -0700

    Implement Chatbox feature
    Add CRUD chatbox

--- base/models.py
+++ base/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+
 class Topic(models.Model):
     name = models.CharField(max_length=200)
 
@@ -11,26 +12,35 @@ class Topic(models.Model):
 
 class Room(models.Model):
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)     #allow null in db
+    topic = models.ForeignKey(
+        Topic, on_delete=models.SET_NULL, null=True)  # allow null in db
     name = models.CharField(max_length=200)
-    description = models.TextField(null=True, blank= True) #null is set to true, can be blank
-    #participants = 
-    update = models.DateTimeField(auto_now =True) #auto_now : everytimes runs update its gonna take a timestamp
-    created = models.DateTimeField(auto_now_add=True) #auto_now_add : know when room created
+    # null is set to true, can be blank
+    description = models.TextField(null=True, blank=True)
+    participants = models.ManyToManyField(
+        User, related_name='participants', blank=True)
+    # auto_now : everytimes runs update its gonna take a timestamp
+    update = models.DateTimeField(auto_now=True)
+    # auto_now_add : know when room created
+    created = models.DateTimeField(auto_now_add=True)
 
-    #This is used to put the newest room on top
+    # This is used to put the newest room on top
     class Meta:
         ordering = ['-update', '-created']
 
     def __str__(self):
         return self.name
 
+
 class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    room = models.ForeignKey(Room, on_delete=models.CASCADE)   #one (project) to many (reviews) relationship
+    # one (project) to many (reviews) relationship
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
     body = models.TextField()
-    update = models.DateTimeField(auto_now =True) #auto_now : everytimes runs update its gonna take a timestamp
-    created = models.DateTimeField(auto_now_add=True) #auto_now_add : know when room created
-    
+    # auto_now : everytimes runs update its gonna take a timestamp
+    update = models.DateTimeField(auto_now=True)
+    # auto_now_add : know when room created
+    created = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
-        return self.body[0:100] # message limit
+        return self.body[0:100]  # message limit
--- base/views.py
+++ base/views.py
@@ -1,7 +1,7 @@
 from threading import ExceptHookArgs
 from django.shortcuts import render, redirect
 from django.db.models import Q
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 from django.contrib.auth.models import User
 from django.contrib import messages
@@ -17,8 +17,9 @@ from django.contrib.auth.forms import UserCreationForm
 #     {'id':5, 'name':'Lets learn C++!'},
 # ]
 
+
 def loginPage(request):
-    page= 'login'
+    page = 'login'
     if request.user.is_authenticated:
         return redirect('home')
 
@@ -27,23 +28,25 @@ def loginPage(request):
         password = request.POST.get('password')
 
         try:
-            user= User.objects.get(username=username)
+            user = User.objects.get(username=username)
         except:
             messages.error(request, 'User does not exist')
 
-        user= authenticate(request, username=username, password=password)
+        user = authenticate(request, username=username, password=password)
 
         if user is not None:
             login(request, user)
             return redirect('home')
         else:
             messages.error(request, 'Username or password does not exist')
-    context = {'page':page}
-    return render(request, 'base/login_register.html',context)
+    context = {'page': page}
+    return render(request, 'base/login_register.html', context)
+
 
 def logoutUser(request):
     logout(request)
     return redirect('home')
+
 
 def registerPage(request):
     #page = 'register'
@@ -51,55 +54,72 @@ def registerPage(request):
 
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
-        if form.is_valid() :
+        if form.is_valid():
             user = form.save(commit=False)
-            user.username = user.username.lower() #clean database
+            user.username = user.username.lower()  # clean database
             user.save()
             login(request, user)
             return redirect('home')
         else:
-            messages.error(request,'Registration error')
-            
-    return render(request,'base/login_register.html', {'form':form})
+            messages.error(request, 'Registration error')
+
+    return render(request, 'base/login_register.html', {'form': form})
+
 
 def home(request):
-    q= request.GET.get('q') if request.GET.get('q') != None else ''
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
 
     rooms = Room.objects.filter(
         Q(topic__name__icontains=q) |
         Q(name__icontains=q) |
         Q(description__icontains=q)
-        )
+    )
 
     topics = Topic.objects.all()
     room_count = rooms.count()
-    context ={'rooms': rooms,'topics': topics, 'room_count': room_count}
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
     return render(request, 'base/home.html', context)
 
+
 def room(request, pk):
-    room = Room.objects.get(id=pk) #needs to be unique or will throw errors
-    #loop through the room values
+    room = Room.objects.get(id=pk)  # needs to be unique or will throw errors
+    # loop through the room values
     # for i in rooms:
     #     if i['id'] == int(pk):  #just to be safe pk might be string
     #         room = i
-    context = {'room': room}
+    room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
+
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room', pk=room.id)
+
+    context = {'room': room, 'room_messages': room_messages,
+               'participants': participants}
     return render(request, 'base/room.html', context)
+
 
 @login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
 
-    if request.method == 'POST' :
-        form= RoomForm(request.POST)
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('home')
 
     context = {'form': form}
-    return render(request,'base/room_make.html', context)
+    return render(request, 'base/room_make.html', context)
+
 
 @login_required(login_url='login')
-def updateRoom(request,pk):
+def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     # this form will be pre filled
     form = RoomForm(instance=room)
@@ -107,17 +127,18 @@ def updateRoom(request,pk):
     if request.user != room.host:
         return HttpResponse('Nope, you are not allowed here')
 
-    if request.method == 'POST' :
-        form= RoomForm(request.POST, instance=room)
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
         if form.is_valid():
             form.save()
             return redirect('home')
 
     context = {'form': form}
-    return render(request,'base/room_make.html', context)
+    return render(request, 'base/room_make.html', context)
+
 
 @login_required(login_url='login')
-def deleteRoom(request,pk):
+def deleteRoom(request, pk):
     room = Room.objects.get(id=pk)
 
     if request.user != room.host:
@@ -126,4 +147,17 @@ def deleteRoom(request,pk):
     if request.method == 'POST':
         room.delete()
         return redirect('home')
-    return render(request, 'base/delete.html', {'obj':room})
+    return render(request, 'base/delete.html', {'obj': room})
+
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse('Nope, you are not allowed here')
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj': message})
